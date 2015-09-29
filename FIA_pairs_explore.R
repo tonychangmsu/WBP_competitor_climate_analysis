08760a26f17bf4b0d24403a9d54d39b0cac26ec5
tonychangmsu <tony.chang@msu.montana.edu>
2015-09-29 14:39:11 -0600

    Added variable selection document

--- FIA_pairs_explore.R
+++ FIA_pairs_explore.R
@@ -22,15 +22,15 @@ FIA_subset = fread(filename, sep = ",", showProgress=T)
 #This procedure should repeat for the various elevation zones
 
 #first query all trees plots where elevation is >10000
-#fs = sqldf(sprintf("SELECT * FROM FIA_subset WHERE ELEV > 10000"))
+fs = sqldf(sprintf("SELECT * FROM FIA_subset WHERE ELEV > 8200"))
 #second query all trees plots where elevation is 8800>elev>9500
 #fs = sqldf(sprintf("SELECT * FROM FIA_subset WHERE ELEV > 8800 AND ELEV < 9500"))
 ###third query all trees plots where elevation is elev < 8500
-fs = sqldf(sprintf("SELECT * FROM FIA_subset WHERE ELEV < 8500"))
+#fs = sqldf(sprintf("SELECT * FROM FIA_subset WHERE ELEV < 8500"))
 n_fs = dim(fs)[1]
 #get a plot of the pairwise values for whitebark pine at class 4 versus class 4 ABLA and PIEN
 #from the first subset we need class 4 WBP, class 4 ABLA and class 4 PIEN
-pairs(PIAL_CLASS4~ABLA_CLASS4+PIEN_CLASS4+ABLA_CLASS3+PIEN_CLASS3+ABLA_CLASS2+PIEN_CLASS2+ABLA_CLASS1+PIEN_CLASS1, data = fs, main = "Scatterplot Matrix (Below 8500' elevation)")
+pairs(PIAL_CLASS4~ABLA_CLASS4+PIEN_CLASS4+ABLA_CLASS3+PIEN_CLASS3+ABLA_CLASS2+PIEN_CLASS2+ABLA_CLASS1+PIEN_CLASS1, data = fs, main = "Scatterplot Matrix (Above 10000' elevation)")
 
 colnames = c("PIAL_CLASS4","ABLA_CLASS4","PIEN_CLASS4","ABLA_CLASS3","PIEN_CLASS3","ABLA_CLASS2","PIEN_CLASS2","ABLA_CLASS1","PIEN_CLASS1")
 fsdata = data.frame(fs$PIAL_CLASS4,fs$ABLA_CLASS4,fs$PIEN_CLASS4,fs$ABLA_CLASS3,fs$PIEN_CLASS3,fs$ABLA_CLASS2, fs$PIEN_CLASS2,fs$ABLA_CLASS1,fs$PIEN_CLASS1)
---
+++ Model_subsetting.md
@@ -0,0 +1,32 @@
+#Variable selection and subsetting
+#Author: Tony Chang
+#Date: 09.29.2015
+
+####Abstract:
+AH: 	"Subset the data such that we can explore the relationships of the predictor variables and basal area for 
+		PIAL"
+
+Division, elevation division = mean(data$ELEV) - sd(data$ELEV) = 8234
+N = 876
+
+Abiotic effects:
+
+1)	TMIN_6
+2)	PPT_4
+3)	SOILM_6
+4)	TMAX_8
+5)	VPD_8
+6)	PACK_4
+7)	ROCKVOL_L1
+8)	SAND_L1
+9)	WHC_100
+	
+Biotic effects:
+10)	ABLA_BA_TOTAL
+11)	OTHER_BA_TOTAL = ALL_BA_TOTAL - ABLA_BA_TOTAL - PIAL_BA_TOTAL
+	
+Interactions:
+12)	ABLA_BA_TOTAL * TMAX_8
+13)	ABLA_BA_TOTAL * PACK_4
+14)	OTHER_BA_TOTAL * WHC_100
+15)	PIEN_BA_TOTAL * VPD_8
